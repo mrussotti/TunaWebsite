c18886dbe0c804600f575a6f145e97ae9b819d75
mrussotti <matthewlax24@yahoo.com>
2023-08-15 13:26:03 -0400

    added wallet connect feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import styles from "./navbar.module.css"; // Import the new CSS file
+import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit'
+
+const rdt = RadixDappToolkit({
+  dAppDefinitionAddress:
+    'account_tdx_d_12xjtdhuzjluel8p03vw35thc3d08w0da8ujde2dpcplg6tyfcnc943',
+  networkId: 13,
+})
 
 export default function Navbar() {
   return (
+    <>
     <nav className={styles.nav}>
       <Link to="/" className={styles.siteTitle}>
         Bigger Tuna Crypto
       </Link>
+      <radix-connect-button />
+
     </nav>
+    </>
   );
 }
 
