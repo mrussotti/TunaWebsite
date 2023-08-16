e2f8540ee2493b857418bf8bec1740127abee555
mrussotti <matthewlax24@yahoo.com>
2023-08-16 15:57:52 -0400

    removed warnings and fixed button z index

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,42 @@
-import React from "react";
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
-import styles from "./navbar.module.css"; // Import the new CSS file
-import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit'
+import React, { useEffect } from "react"; // Added useEffect
+import { Link } from "react-router-dom";
+import styles from "./navbar.module.css";
+import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit';
 
 const rdt = RadixDappToolkit({
   dAppDefinitionAddress:
     'account_tdx_d_12xjtdhuzjluel8p03vw35thc3d08w0da8ujde2dpcplg6tyfcnc943',
   networkId: 13,
-})
+});
 
 export default function Navbar() {
+  // Use the useEffect hook to set the button's theme and mode on component mount
+  useEffect(() => {
+    rdt.buttonApi.setTheme('black');
+    rdt.buttonApi.setMode('dark');
+  }, []); // The empty dependency array ensures that this runs only once, similar to componentDidMount.
+
   return (
     <>
-    <nav className={styles.nav}>
-      <Link to="/" className={styles.siteTitle}>
-        Bigger Tuna Crypto
-      </Link>
-      <radix-connect-button />
-
-    </nav>
+      <nav className={styles.nav}>
+        <Link to="/" className={styles.siteTitle}>
+          Bigger Tuna Crypto
+        </Link>
+        <radix-connect-button style={{ zIndex: 100 }} />
+      </nav>
     </>
   );
 }
 
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-  return (
-    <li className={isActive ? styles.active : ""}>
-      <Link to={to} className={styles.btn} {...props}>
-        {children}
-      </Link>
-    </li>
-  );
-}
+
+// function CustomLink({ to, children, ...props }) {
+//   const resolvedPath = useResolvedPath(to);
+//   const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+//   return (
+//     <li className={isActive ? styles.active : ""}>
+//       <Link to={to} className={styles.btn} {...props}>
+//         {children}
+//       </Link>
+//     </li>
+//   );
+// }
